64742141f5694023d4e4525cc0eb3ab805d22cc7
William9923 <williamong1400@gmail.com>
2022-07-28 18:03:42 +0700

    aerial adjustment

--- lua/user/aerial.lua
+++ lua/user/aerial.lua
@@ -2,13 +2,19 @@
 -- Aerial (tag viewer) configuration file
 -----------------------------------------------------------
 
--- Plugin: aerial 
+-- Plugin: aerial
 -- url: https://github.com/stevearc/aerial.nvim
 
 local status_ok, aerial = pcall(require, "aerial")
 if not status_ok then
-  return
+	return
 end
 
-aerial.setup({})
-
+aerial.setup({
+	ignore = {
+		-- List of filetypes to ignore.
+		filetypes = {
+			"markdown",
+		},
+	},
+})
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,17 +1,41 @@
-local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+-----------------------------------------------------------
+-- Treesitter configuration file
+----------------------------------------------------------
+
+-- Plugin: nvim-treesitter
+-- url: https://github.com/nvim-treesitter/nvim-treesitter
+
+
+local status_ok, nvim_treesitter = pcall(require, 'nvim-treesitter.configs')
 if not status_ok then
-	return
+  return
 end
 
-configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
-})
+-- See: https://github.com/nvim-treesitter/nvim-treesitter#quickstart
+nvim_treesitter.setup {
+  autopairs = {
+    enable = true,
+  },
+  -- A list of parser names, or "all"
+  ensure_installed = {
+    -- Lang
+    'html', 'css', 'c', 'cpp', 'javascript', 'typescript', 'python', 'go', 'rust',
+    -- Filetype specific
+    'json', 'toml', 'markdown',
+    -- Bash & script
+    'bash', 'lua', 'vim', 'dockerfile', 'fish',
+  },
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+    additional_vim_regex_highlighting = true,
+  },
+  indent = { enable = true, disable = {"yaml", "python", "css"}},
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  }
+}
+
